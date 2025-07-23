b4d4a59d63eeb544842abeabdee41617d1c01607
Chirplett <chirplett@gmail.com>
2025-07-23 21:50:51 +0900

    feat: implement draw number selection using UIPickerView
    - Added drawNumberPickerView and drawNumbers array to support picker data
    - Connected drawNumberPickerView as inputView to drawNumberTextField
    - Implemented UIPickerViewDelegate and DataSource methods: numberOfComponents, numberOfRowsInComponent, titleForRow, didSelectRow
    - Updated drawNumberTextField text on selection and dismissed picker automatically

--- NetworkStarter/LottoViewController.swift
+++ NetworkStarter/LottoViewController.swift
@@ -12,25 +12,40 @@ class LottoViewController: UIViewController {
     
     private let drawNumberTextField: UITextField = {
         
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        textField.placeholder = "회차를 선택해 주세요"
-        textField.textAlignment = .center
-        return textField
+        let drawNumberTextField = UITextField()
+        drawNumberTextField.borderStyle = .roundedRect
+        drawNumberTextField.placeholder = "회차를 선택해 주세요"
+        drawNumberTextField.textAlignment = .center
+        return drawNumberTextField
         
+    }()
+    
+    private let drawNumberPickerView: UIPickerView = {
+        let drawNumberPickerView = UIPickerView()
+        return drawNumberPickerView
+        
+    }()
+    
+    private let drawNumbers = {
+        var numbers: [Int] = []
+        for number in 1...1181 {
+            numbers.append(number)
+        }
+        return numbers
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        drawNumberPickerView.delegate = self
+        drawNumberPickerView.dataSource = self
+        drawNumberTextField.inputView = drawNumberPickerView
         
         configureHierarchy()
         configureLayout()
         configureView()
         
     }
-    
-    
-    
 
 }
 
@@ -53,5 +68,28 @@ extension LottoViewController: ViewDesignProtocol {
             make.height.equalTo(44)
         }
     }
+    
+}
+
+extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return drawNumbers.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(drawNumbers[row])"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedNumber = drawNumbers[row]
+        drawNumberTextField.text = "\(selectedNumber)"
+        view.endEditing(true)
+    }
+    
     
 }

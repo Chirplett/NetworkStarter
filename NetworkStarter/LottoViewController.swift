86b29d7e54d19a01d9eabd60dcae23e0d8f84159
Chirplett <chirplett@gmail.com>
2025-07-23 21:08:04 +0900

    feat: implement main ViewController with Lotto navigation and layout
    - Created ViewController as main entry point
    - Added Lotto button with SnapKit-based layout
    - Connected button to push LottoViewController via navigation
    - Applied ViewDesignProtocol to manage view setup

---
+++ NetworkStarter/LottoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LottoViewController.swift
+//  NetworkStarter
+//
+//  Created by Jude Song on 7/23/25.
+//
+
+import UIKit
+
+class LottoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+}
--- NetworkStarter/ViewController.swift
+++ NetworkStarter/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    private let goToLottoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Lotto", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.darkGray.cgColor
+        button.layer.backgroundColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureHierarchy()
+        configureLayout()
+        configureView()
+        
         
     }
+    
+    @objc
+    private func goToLottoButtonTapped(_ sender: UIButton) {
+        let viewController = LottoViewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+}
+
+extension ViewController: ViewDesignProtocol {
+    
+    func configureHierarchy() {
+        view.addSubview(goToLottoButton)
+    }
+    
+    func configureView() {
+        view.backgroundColor = .white
+        goToLottoButton.addTarget(self, action: #selector(goToLottoButtonTapped), for: .touchUpInside)
+    }
+    
+    func configureLayout() {
+        goToLottoButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(32)
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    
+   
 
 
 }

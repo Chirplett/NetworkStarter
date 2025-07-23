cac472bc52fe2aa6a06494830a3fbcd68dd002cc
Chirplett <chirplett@gmail.com>
2025-07-23 21:26:06 +0900

    feat: create LottoViewController with draw number text field
    - Added drawNumberTextField with placeholder and center alignment
    - Set up layout using SnapKit with horizontal insets and top margin
    - Applied ViewDesignProtocol for structured view setup
    - Configured navigation title as "Lotto"

--- NetworkStarter/LottoViewController.swift
+++ NetworkStarter/LottoViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class LottoViewController: UIViewController {
+    
+    private let drawNumberTextField: UITextField = {
+        
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "회차를 선택해 주세요"
+        textField.textAlignment = .center
+        return textField
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+        
     }
     
+    
+    
 
+}
+
+extension LottoViewController: ViewDesignProtocol {
+    
+    func configureHierarchy() {
+        view.addSubview(drawNumberTextField)
+    }
+    
+    func configureView() {
+        navigationItem.title = "Lotto"
+        view.backgroundColor = .white
+        
+    }
+    
+    func configureLayout() {
+        drawNumberTextField.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(32)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.height.equalTo(44)
+        }
+    }
+    
 }

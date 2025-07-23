a817bcae8b92149ed0b651a7e7da3ca4e14c74e8
Chirplett <chirplett@gmail.com>
2025-07-23 20:47:00 +0900

    chore: set up initial project with SnapKit and view structure protocol
    - Initialized project without storyboard using code-based UI
    - Installed SnapKit via Swift Package Manager
    - Created ViewDesignProtocol for consistent view setup
    - Applied protocol to LottoViewController with configureView, configureHierarchy, and configureLayout methods

---
+++ NetworkStarter/ViewDesignProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  ViewDesignProtocol.swift
+//  NetworkStarter
+//
+//  Created by Jude Song on 7/23/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol {
+    
+    func configureHierarchy()
+    func configureView()
+    func configureLayout()
+    
+}
